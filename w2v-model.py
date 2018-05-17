84f605fbc391513e7d10918897284f4a6406aea5
tkr <kgtkr.jp@gmail.com>
2018-05-17 21:12:46 +0900

    omosugiィ

--- w2v-model.py
+++ w2v-model.py
@@ -11,6 +11,7 @@ def readDat():
     dir = "dat/news4vip"
     data = []
     for file in os.listdir(dir)[:100]:
+        print(file)
         path = f"{dir}/{file}"
         with open(path, "r") as f:
             reses = [text.text(x[6]) for x in json.loads(f.read())["comments"]]
@@ -22,13 +23,12 @@ def readDat():
 # http://own-search-and-study.xyz/2017/10/08/mecab%E3%81%A8gensim%E3%81%A7word2vec%E3%83%A2%E3%83%87%E3%83%AB%E3%82%92%E5%AD%A6%E7%BF%92%E3%81%99%E3%82%8B/
 def tokenize(sentence):
     try:
-        """data = mecab.parse(sentence)
+        data = mecab.parse(sentence)
         data = io.StringIO(data)
         data = pd.read_csv(data, sep='\t', header=None)
         # 名詞のみを使う場合。
         data = data.loc[(data[3].str.find("名詞") >= 0)]
         texts = list(data.iloc[:, 0])
-        return texts"""
         return mecab.parse(sentence)
     except:
         return []
